0def7647191bb57624e886cc618a5f9fe4f3f4e9
cgmbauer <cgmbauer@gmail.com>
2020-07-08 20:14:18 -0300

    server.ts done

---
+++ backend/src/server.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+
+import 'express-async-errors';
+
+import express, { Request, Response, NextFunction } from 'express';
+import routes from './routes';
+
+import './database';
+
+import uploadConfig from './config/upload';
+
+import AppError from './errors/AppError';
+
+const app = express();
+
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
+app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+    console.error(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error.',
+    });
+  },
+);
+
+app.listen(3333, () => {
+  console.log('Server started on 3333!');
+});
