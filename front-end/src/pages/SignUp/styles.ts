291d433e6de401af36259ad90bd6453f24d34f33
cgmbauer <cgmbauer@gmail.com>
2020-07-20 12:53:40 -0300

    signup styles done

---
+++ front-end/src/pages/SignUp/styles.ts
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import signUpBackgroundImg from '../../assets/sign-up-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+  width: 100%;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+  }
+
+  a {
+    color: #f4ede8;
+    text-decoration: none;
+
+    display: block;
+    margin-top: 24px;
+
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      color: ${shade(0.2, '#f4ede8')};
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signUpBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
