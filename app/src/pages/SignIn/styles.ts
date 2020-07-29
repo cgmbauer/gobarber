8851b366cef413adf19da4d4dfe5627f3fddc233
cgmbauer <cgmbauer@gmail.com>
2020-07-29 13:37:19 -0300

    Container and Title styles

---
+++ app/src/pages/SignIn/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+
+  padding: 0 30px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
+`;
