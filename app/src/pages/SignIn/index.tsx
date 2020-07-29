cefc18f719dc05fe00bef8b7ff44880bdcfaef43
cgmbauer <cgmbauer@gmail.com>
2020-07-29 17:43:18 -0300

    scrollview, keyboardAvoidingView, keyboardDidShow

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -1,30 +1,88 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
+  const [keyboardDidShow, setKeyboardDidSHow] = useState<boolean>(false);
+
+  const keyboardInteraction = useCallback(() => {
+    setKeyboardDidSHow(!keyboardDidShow);
+  }, [keyboardDidShow]);
+
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', keyboardInteraction);
+    Keyboard.addListener('keyboardDidHide', keyboardInteraction);
+
+    return () => {
+      Keyboard.removeListener('keyboardDidShow', keyboardInteraction);
+      Keyboard.removeListener('keyboardDidHide', keyboardInteraction);
+    };
+  }, [keyboardInteraction]);
+
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title> Faça seu logon </Title>
-
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
-
-      <Button
-        onPress={() => {
-          console.log('Foi');
-        }}
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
       >
-        Entrar
-      </Button>
-    </Container>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title> Faça seu logon </Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('Foi');
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {!keyboardDidShow && (
+        <CreateAccountButton>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      )}
+    </>
   );
 };
 
