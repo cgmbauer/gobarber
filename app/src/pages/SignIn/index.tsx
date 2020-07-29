6def672dbb3b10e1f1c589cd82446bc3280a4ccb
cgmbauer <cgmbauer@gmail.com>
2020-07-29 13:36:42 -0300

    Input and button are set

---
+++ app/src/pages/SignIn/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title> Faça seu logon </Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('Foi');
+        }}
+      >
+        Entrar
+      </Button>
+    </Container>
+  );
+};
+
+export default SignIn;
