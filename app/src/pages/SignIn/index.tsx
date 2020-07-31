331d117357beb30817626262b52a42bef8f6095e
cgmbauer <cgmbauer@gmail.com>
2020-07-30 21:18:18 -0300

    signin page done

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
+
 import {
   Image,
   KeyboardAvoidingView,
@@ -6,8 +7,20 @@ import {
   View,
   ScrollView,
   Keyboard,
+  TextInput,
+  Alert,
 } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+
+import { FormHandles } from '@unform/core';
+
+import * as Yup from 'yup';
+
 import Icon from 'react-native-vector-icons/Feather';
+import { useAuth } from '../../hooks/auth';
+
+import getValidationErrors from '../../utilis/getValidationErros';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -21,24 +34,80 @@ import {
   ForgotPasswordText,
   CreateAccountButton,
   CreateAccountButtonText,
+  Form,
 } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
+  const navigation = useNavigation();
+
   const [keyboardDidShow, setKeyboardDidSHow] = useState<boolean>(false);
 
-  const keyboardInteraction = useCallback(() => {
-    setKeyboardDidSHow(!keyboardDidShow);
-  }, [keyboardDidShow]);
+  const keyboardShowed = useCallback(() => {
+    setKeyboardDidSHow(true);
+  }, []);
+
+  const keyboardDisappeared = useCallback(() => {
+    setKeyboardDidSHow(false);
+  }, []);
 
   useEffect(() => {
-    Keyboard.addListener('keyboardDidShow', keyboardInteraction);
-    Keyboard.addListener('keyboardDidHide', keyboardInteraction);
+    if (Platform.OS === 'android') {
+      Keyboard.addListener('keyboardDidShow', keyboardShowed);
+      Keyboard.addListener('keyboardDidHide', keyboardDisappeared);
+    } else {
+      return;
+    }
 
     return () => {
-      Keyboard.removeListener('keyboardDidShow', keyboardInteraction);
-      Keyboard.removeListener('keyboardDidHide', keyboardInteraction);
+      Keyboard.removeListener('keyboardDidShow', keyboardShowed);
+      Keyboard.removeListener('keyboardDidHide', keyboardDisappeared);
     };
-  }, [keyboardInteraction]);
+  }, [keyboardShowed, keyboardDisappeared]);
+
+  const { signIn } = useAuth();
+
+  const handleSignIn = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório.')
+            .email('Digite um e-mail válido.'),
+          password: Yup.string().required('Senha obrigatória.'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await signIn({ email: data.email, password: data.password });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        Alert.alert(
+          'Erro na autenticação',
+          'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        );
+      }
+    },
+    [signIn],
+  );
 
   return (
     <>
@@ -58,16 +127,40 @@ const SignIn: React.FC = () => {
               <Title> Faça seu logon </Title>
             </View>
 
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardType="email-address"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+              />
 
-            <Button
-              onPress={() => {
-                console.log('Foi');
-              }}
-            >
-              Entrar
-            </Button>
+              <Input
+                ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm();
+                }}
+              />
+
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Entrar
+              </Button>
+            </Form>
 
             <ForgotPassword>
               <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
@@ -77,7 +170,7 @@ const SignIn: React.FC = () => {
       </KeyboardAvoidingView>
 
       {!keyboardDidShow && (
-        <CreateAccountButton>
+        <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
           <Icon name="log-in" size={20} color="#ff9000" />
           <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
         </CreateAccountButton>
