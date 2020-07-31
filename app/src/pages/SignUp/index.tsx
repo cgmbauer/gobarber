60df7a78011eda126d69414dee39a27e1b3756a1
cgmbauer <cgmbauer@gmail.com>
2020-07-30 21:19:48 -0300

    signup page done

--- app/src/pages/SignUp/index.tsx
+++ app/src/pages/SignUp/index.tsx
@@ -1,9 +1,193 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+  Keyboard,
+  TextInput,
+  Alert,
+} from 'react-native';
 
-import { Container } from './styles';
+import { FormHandles } from '@unform/core';
+
+import * as Yup from 'yup';
+
+import { useNavigation } from '@react-navigation/native';
+
+import Icon from 'react-native-vector-icons/Feather';
+import api from '../../services/api';
+
+import getValidationErrors from '../../utilis/getValidationErros';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  BackToSignIn,
+  BackToSignInText,
+  Form,
+} from './styles';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const formRef = useRef<FormHandles>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+  const emailInputRef = useRef<TextInput>(null);
+
+  const navigation = useNavigation();
+
+  const [keyboardDidShow, setKeyboardDidSHow] = useState<boolean>(false);
+
+  const keyboardShowed = useCallback(() => {
+    setKeyboardDidSHow(true);
+  }, []);
+
+  const keyboardDisappeared = useCallback(() => {
+    setKeyboardDidSHow(false);
+  }, []);
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      Keyboard.addListener('keyboardDidShow', keyboardShowed);
+      Keyboard.addListener('keyboardDidHide', keyboardDisappeared);
+    } else {
+      return;
+    }
+
+    return () => {
+      Keyboard.removeListener('keyboardDidShow', keyboardShowed);
+      Keyboard.removeListener('keyboardDidHide', keyboardDisappeared);
+    };
+  }, [keyboardShowed, keyboardDisappeared]);
+
+  const handleSignUp = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório.'),
+          email: Yup.string()
+            .required('E-mail obrigatório.')
+            .email('Digite um e-mail válido.'),
+          password: Yup.string().min(6, 'No mínimo 6 dígitos.'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('/users', data);
+
+        Alert.alert(
+          'Cadastro relizado!',
+          'Você já pode fazer seu logon no GoBarber.',
+        );
+
+        navigation.goBack();
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        Alert.alert(
+          'Erro no cadastro.',
+          'Ocorreu um erro ao fazer o cadastro, tente novamente.',
+        );
+      }
+    },
+    [navigation],
+  );
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title> Crie sua conta </Title>
+            </View>
+            <Form ref={formRef} onSubmit={handleSignUp}>
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={emailInputRef}
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={passwordInputRef}
+                secureTextEntry
+                textContentType="newPassword"
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                returnKeyType="send"
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm();
+                }}
+              />
+
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Entrar
+              </Button>
+            </Form>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {!keyboardDidShow && (
+        <BackToSignIn onPress={() => navigation.goBack()}>
+          <Icon name="arrow-left" size={20} color="#fff" />
+          <BackToSignInText>Voltar</BackToSignInText>
+        </BackToSignIn>
+      )}
+    </>
+  );
 };
 
 export default SignUp;
