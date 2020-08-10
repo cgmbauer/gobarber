4bb7a530462d1bc784946b509017a6ec9c6cdcea
cgmbauer <cgmbauer@gmail.com>
2020-08-10 20:38:18 -0300

    changed folders: DDD

---
+++ backend/src/modules/appointments/infra/typeorm/entities/Appointments.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import User from '@modules/users/infra/typeorm/entities/Users';
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
+
+  @Column()
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Appointment;
