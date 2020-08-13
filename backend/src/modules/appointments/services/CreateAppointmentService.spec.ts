6b847fb053059aa1a8f3ef981b5951be01869a01
cgmbauer <cgmbauer@gmail.com>
2020-08-12 23:45:26 -0300

    Test for CreateAppointmentsService

---
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,26 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmnet = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmnet.execute({
+      date: new Date(),
+      provider_id: '123456789',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456789');
+  });
+});
+
+// describe('CreateAppointment', () => {
+//   it('it should not be able to create two appointments on the sime time', () => {
+
+//   })
+// })
