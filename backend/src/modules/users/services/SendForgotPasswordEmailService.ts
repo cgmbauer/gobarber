5d67ac11cd2f4c01451f23f4973c9ec3ee567a96
cgmbauer <cgmbauer@gmail.com>
2020-08-19 19:08:28 -0300

    SendForgotPasswordEmailService created

---
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,41 @@
+import { inject, injectable } from 'tsyringe';
+
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+interface IRequestDTO {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ email }: IRequestDTO): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exist.');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido.',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
