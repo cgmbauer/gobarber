b306c3c80b15bd63c6caf82aa94c4590e20c0094
cgmbauer <cgmbauer@gmail.com>
2020-08-22 20:28:27 -0300

    Created ShowProfileService

---
+++ backend/src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/errors/AppError';
+
+import { inject, injectable } from 'tsyringe';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+
+import User from '../infra/typeorm/entities/Users';
+
+interface IRequestDTO {
+  user_id: string;
+}
+
+@injectable()
+class ShowProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ user_id }: IRequestDTO): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
