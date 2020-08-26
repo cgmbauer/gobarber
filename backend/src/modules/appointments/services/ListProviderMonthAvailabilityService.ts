ee39c3155a7b930e56510e4e04a8516d6e85060a
cgmbauer <cgmbauer@gmail.com>
2020-08-26 16:44:30 -0300

    fixed class name

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -15,7 +15,7 @@ type IResponse = Array<{
 }>;
 
 @injectable()
-class ListProviderMothAvailabilityService {
+class ListProviderMonthAvailabilityService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
@@ -56,4 +56,4 @@ class ListProviderMothAvailabilityService {
   }
 }
 
-export default ListProviderMothAvailabilityService;
+export default ListProviderMonthAvailabilityService;
