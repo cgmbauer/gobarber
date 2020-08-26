104b100f2506ad2c21acf33b6c6610fa638d5771
cgmbauer <cgmbauer@gmail.com>
2020-08-26 16:26:47 -0300

    findAllInMonth & findAllInDay added

--- backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,15 @@
 import Appointments from '../infra/typeorm/entities/Appointments';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProvider from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProvider from '../dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointments>;
   findByDate(date: Date): Promise<Appointments | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthFromProvider,
+  ): Promise<Appointments[]>;
+  findAllInDayFromProvider(
+    data: IFindAllInDayFromProvider,
+  ): Promise<Appointments[]>;
 }
