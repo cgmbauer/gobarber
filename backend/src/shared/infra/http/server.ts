66f427d67f0a0ca60bc164545d803139426e091c
cgmbauer <cgmbauer@gmail.com>
2020-08-27 16:27:14 -0300

    import dotenv and celebrate "errors"

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import express, { Request, Response, NextFunction } from 'express';
 
@@ -21,6 +23,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
