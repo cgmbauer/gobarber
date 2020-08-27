870d89e6a0c7923631f047a680976e06cb7f992f
cgmbauer <cgmbauer@gmail.com>
2020-08-27 16:29:00 -0300

    Celebrate validation

--- backend/src/modules/users/infra/http/routes/profile.routes.ts
+++ backend/src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import ProfileController from '../controllers/ProfileController';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
@@ -12,6 +14,18 @@ profileRouter.use(ensureAuthenticated);
 
 profileRouter.get('/', profileController.show);
 
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
