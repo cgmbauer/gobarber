496d89cf03bf347de4488a985530e71a081af95b
cgmbauer <cgmbauer@gmail.com>
2020-08-27 16:31:14 -0300

    Created ListProviderAppointmentsService

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,40 @@
+import { inject, injectable } from 'tsyringe';
+
+import Appointment from '../infra/typeorm/entities/Appointments';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequestDTO {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+@injectable()
+class ListProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IRequestDTO): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        day,
+        month,
+        year,
+      },
+    );
+
+    return appointments;
+  }
+}
+
+export default ListProviderAppointmentsService;
